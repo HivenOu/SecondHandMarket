eab2f1a465c510cb5decaf43b6a8d17bb1843245
guiyong.ou <guiyong.ou@daocloud.io>
2023-03-06 10:42:15 +0800

    fix

--- src/main/java/com/secondhandmarket/App.java
+++ src/main/java/com/secondhandmarket/App.java
@@ -2,6 +2,7 @@ package com.secondhandmarket;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -11,8 +12,12 @@ import org.springframework.context.annotation.Bean;
 @EnableSwagger2Doc
 @MapperScan("com.secondhandmarket.mapper")
 public class App {
+    public static void main(String[] args) {
+     /*   new SpringApplicationBuilder(App.class).listeners(new TestListener())
+                        .initializers(new TestInitializer()).run(args);*/
+        SpringApplication.run(App.class,args);
 
-
+    }
     @Bean
     ApplicationListener<ApplicationReadyEvent> getListener(){
         return applicationReadyEvent -> System.out.println("this is a lambda listener"+applicationReadyEvent.getApplicationContext().getId());
