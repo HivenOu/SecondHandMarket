f0c08ffdfa2be12bf52d5f34e8bfd000be2dd265
guiyong.ou <guiyong.ou@daocloud.io>
2023-03-16 11:50:13 +0800

    :fire: feat log

--- src/main/java/com/secondhandmarket/App.java
+++ src/main/java/com/secondhandmarket/App.java
@@ -4,9 +4,6 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableSwagger2Doc
@@ -16,10 +13,5 @@ public class App {
      /*   new SpringApplicationBuilder(App.class).listeners(new TestListener())
                         .initializers(new TestInitializer()).run(args);*/
         SpringApplication.run(App.class,args);
-
-    }
-    @Bean
-    ApplicationListener<ApplicationReadyEvent> getListener(){
-        return applicationReadyEvent -> System.out.println("this is a lambda listener"+applicationReadyEvent.getApplicationContext().getId());
     }
 }
